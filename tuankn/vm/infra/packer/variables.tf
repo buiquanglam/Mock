af75ce3d8da2eb030f6afc6eb609890d9127e4c0
KimNgocTuan <daitintdk@gmail.com>
2023-04-05 18:28:54 +0700

    add file terraform

---
+++ tuankn/vm/infra/packer/variables.tf
@@ -0,0 +1,54 @@
+variable "project" {
+  default = "packer-tuankn"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "profile" {
+  default = "packer-demo"
+}
+
+variable "vpc_name" {
+  default = "packer-vpc"
+}
+
+variable "vpc_cidr" {
+  default = "192.168.100.0/24"
+}
+
+variable "public_subnet_cidr" {
+  default = {
+    "0" : "192.168.100.16/28"
+    "1" : "192.168.100.32/28"
+    "2" : "192.168.100.48/28"
+  }
+}
+
+variable "build_number" {
+  description = "Jenkin build number"
+  default     = 1
+}
+
+variable "private_keyname" {
+  description = "private keyname"
+  default     = "packer_id_rsa"
+}
+
+variable "ami_spec" {
+  description = "AMI spec"
+  default = {
+    "instance_type"      = "t3.micro"
+    "ami_desc"           = "Canonical, Ubuntu, 18.04 LTS*"
+    "ami_owner"          = "099720109477"
+    "volume_type"        = "gp2"
+    "volume_size"        = "20"
+    "packer_remote_user" = "ubuntu"
+  }
+}
+
+variable "ansible_dir_path" {
+  description = "Ansible dir path"
+  default     = "../.."
+}
