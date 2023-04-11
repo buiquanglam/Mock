d8c707b6469b0aafbae6f83fa1c69ebf0541cf54
anhht57 <anhht@inet.vn>
2023-04-11 14:20:08 +0700

    fix

--- anhht57/app/database.go
+++ anhht57/app/database.go
@@ -2,14 +2,15 @@ package app
 
 import (
 	"core/internal/models"
-	// "log"
-	// "os"
-	// "time"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	// "gorm.io/gorm/logger"
+	"gorm.io/gorm/logger"
 )
 
 type DatabaseConfig struct {
@@ -26,15 +27,15 @@ type DatabaseConfig struct {
 
 func (cg *DatabaseConfig) Setup() {
 	logrus.SetLevel(logrus.DebugLevel)
-	// newLogger := logger.New(
-	// 	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-	// 	logger.Config{
-	// 		SlowThreshold:             time.Second,   // Slow SQL threshold
-	// 		LogLevel:                  logger.Silent, // Log level
-	// 		IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
-	// 		Colorful:                  false,         // Disable color
-	// 	},
-	// )
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
 	// mainDbDNS := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", cg.Username, cg.Password, cg.Host, cg.Port, cg.DBName)
 	// DB, err := gorm.Open(
 	// 	postgres.New(postgres.Config{
@@ -45,8 +46,18 @@ func (cg *DatabaseConfig) Setup() {
 	// 		PrepareStmt: true,
 	// 		Logger:      newLogger,
 	// 	})
-    dbURL := "postgres://host:pass@localhost:5432/crud"
-	DB, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+    // dbURL := "postgres://host:pass@localhost:5432/crud"
+	dbURL :=  fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",cg.Host,cg.Username,cg.Password,cg.DBName,cg.Port)
+	DB, err := gorm.Open(
+		postgres.Open(dbURL), 
+		&gorm.Config{
+		    PrepareStmt: true,
+		    Logger:      newLogger,
+	    })
+
+	// dsn := "host=db user=host password=pass dbname=crud port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+    // db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 
 	if cg.Debug {
 		DB = DB.Debug()
